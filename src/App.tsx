a28bae37f678c4716895a050e777fe4ed437d02b
WahyuAndikaRahadi <wahyuandikarahadi19@gmail.com>
2025-12-04 13:24:33 +0700

    update aja

--- src/App.tsx
+++ src/App.tsx
@@ -80,6 +80,7 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/cam" element={<WasteDetection />} />
+            <Route path="/ai" element={<EcoBot />} />
             <Route path="/community-events" element={<CommunityEvents />} />
             <Route path="/community-social" element={<SocialCommunity />} />
             <Route path="/dashboard-admin" element={<AdminDashboard />} />
